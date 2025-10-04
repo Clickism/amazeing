14d2c96eff09a172f7e7a2d4f6170ff81416fa83
Clickism <yagizaktasx@gmail.com>
2025-10-04 22:02:03 +0200

    update code editor

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -1,7 +1,21 @@
 import { Window } from "../ui/Window/Window.tsx";
 import { useTranslation } from "react-i18next";
-import styles from "./CodeEditor.module.css";
 import Editor from "react-simple-code-editor";
+import { Highlight, themes } from "prism-react-renderer";
+import styles from "./CodeEditor.module.css";
+import type { CSSProperties } from "react";
+
+function LineNumbers({ lineNumbers }: { lineNumbers: number }) {
+  return (
+    <div className={styles.lineNumbers}>
+      {Array.from({ length: lineNumbers }).map((_, i) => (
+        <div key={i} className={styles.lineNumber}>
+          {i + 1}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export function CodeEditor({
   code,
@@ -11,19 +25,39 @@ export function CodeEditor({
   setCode: (code: string) => void;
 }) {
   const { t } = useTranslation();
+  const theme = themes.oneDark;
+  const lineNumbers = code.split("\n").length;
   return (
     <Window title={t("codeEditor.title")}>
-      <Editor
-        textareaClassName={styles.codeEditor}
-        highlight={(code) => code}
-        onValueChange={setCode} value={code}
-        padding="1rem"
-        style={{
-          fontFamily: '"Jetbrains Mono", monospace',
-          backgroundColor: 'var(--clr-surface-a0)',
-          height: "100%",
-        }}
-      />
+      <div className={styles.codeEditor} style={{
+        "--foreground-color": theme.plain.color,
+        "--background-color": theme.plain.backgroundColor
+      } as CSSProperties}>
+        <LineNumbers lineNumbers={lineNumbers} />
+        <Editor
+          value={code}
+          onValueChange={setCode}
+          highlight={(code) => (
+            <Highlight theme={theme} code={code} language="js">
+              {({ style, tokens, getLineProps, getTokenProps }) => (
+                <pre style={{ ...style, margin: 0 }}>
+                  {tokens.map((line, i) => (
+                    <div key={i} {...getLineProps({ line, key: i })}>
+                      {line.map((token, key) => (
+                        <span key={key} {...getTokenProps({ token, key })} />
+                      ))}
+                    </div>
+                  ))}
+                </pre>
+              )}
+            </Highlight>
+          )}
+          padding={10}
+          style={{
+            flex: 1,
+          }}
+        />
+      </div>
     </Window>
   );
 }
