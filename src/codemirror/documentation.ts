d9e3c3497714d6f8a1bbf20f7009b2041d0b68f3
Clickism <yagizaktasx@gmail.com>
2025-12-03 18:17:13 +0100

    adds more documentation

--- src/codemirror/documentation.ts
+++ src/codemirror/documentation.ts
@@ -7,6 +7,7 @@ type Documentation = {
 };
 
 type Documentations = {
+  // TODO: Remove optionality
   [K in Instruction["type"]]?: Documentation;
 };
 
@@ -18,17 +19,53 @@ const arithmeticActions = {
   and: "Bitwise ANDs",
   or: "Bitwise ORs",
   xor: "Bitwise XORs",
+  sll: "Shifts left (logical)",
+  srl: "Shifts right (logical)",
+  sra: "Shifts right (arithmetic)",
 };
 
+const comparisonRelations = {
+  lt: "less than",
+  gt: "greater than",
+  lte: "less than or equal to",
+  gte: "greater than or equal to",
+  eq: "equal to",
+  neq: "not equal to"
+}
+
 export const INSTRUCTION_DOCUMENTATIONS: Documentations = {
+  var: {
+    usage: "var <name: identifier>\nvar <name: identifier>[]",
+    description: 'Declares a new variable or array (if "[]" is present) named "name".'
+  },
+  load: {
+    usage: "load <dest: address> <immediate: value>",
+    description: 'Loads the given immediate value into "dest".'
+  },
+  copy: {
+    usage: "copy <dest: address> <src: address>",
+    description: 'Copies the value at address "src" and puts it into "dest".',
+  },
   print: {
     usage: "print <src: address>",
     description: "Outputs the value of an address to the console.",
   },
+  not: {
+    usage: "not <dest: address> <src: address>",
+    description: 'Inverts "src" and puts the result into "dest".',
+  },
+  // Arithmetic
   ...Object.fromEntries(
     Object.entries(arithmeticActions).map(([instr, action]) => [
       instr,
       arithmetic(instr, action),
+    ])
+  ),
+  // Comparison
+  ...Object.fromEntries(
+    Object.entries(comparisonRelations).map(([instr, relation]) => [
+      instr,
+      comparison(instr, relation),
     ])
   ),
 };
@@ -36,6 +73,13 @@ export const INSTRUCTION_DOCUMENTATIONS: Documentations = {
 function arithmetic(instruction: string, action: string): Documentation {
   return {
     usage: `${instruction} <dest: address> <src1: address> <src2: address | integer>`,
-    description: `${action} src1 and src2 and puts the result into dest.`,
+    description: `${action} "src1" and "src2" and puts the result into dest.`,
+  };
+}
+
+function comparison(instruction: string, relation: string): Documentation {
+  return {
+    usage: `${instruction} <dest: address> <src1: address> <src2: address | integer>`,
+    description: `Checks if "src1" is ${relation} "src2" and puts the result into dest.`,
   };
 }
