a4c76b6e166655042f988b79395554f22a0c8f62
Clickism <yagizaktasx@gmail.com>
2026-01-16 20:38:22 +0100

    update color scheme toggle

--- src/components/NavBar/ThemeToggle/ColorSchemeToggle.tsx
+++ src/components/NavBar/ThemeToggle/ColorSchemeToggle.tsx
@@ -1,24 +1,50 @@
-import { BiMoon } from "react-icons/bi";
-import { ImSun } from "react-icons/im";
-import { useColorScheme } from "../../../theme/ColorSchemeContext.tsx";
-import { Button } from "../../Button/Button.tsx";
+import {
+  type ColorScheme,
+  useColorScheme,
+} from "../../../theme/ColorSchemeContext.tsx";
 import { Tooltip } from "../../popup/Tooltip/Tooltip.tsx";
 import { useTranslation } from "react-i18next";
+import styles from "./ColorSchemeToggle.module.css";
+import { motion } from "motion/react";
+import clsx from "clsx";
+import { ImSun } from "react-icons/im";
+import { BiMoon } from "react-icons/bi";
+
+const colorSchemes: ColorScheme[] = ["light", "dark"];
 
 export function ColorSchemeToggle() {
   const { t } = useTranslation();
   const { colorScheme, setColorScheme } = useColorScheme();
   return (
     <Tooltip content={t("colorScheme.toggle")}>
-      <Button
-        shape="icon"
+      <motion.button
+        className={styles.container}
         onClick={() => {
-          setColorScheme(colorScheme === "light" ? "dark" : "light");
+          const nextScheme = colorScheme === "light" ? "dark" : "light";
+          setColorScheme(nextScheme);
         }}
-        variant="transparent"
+        whileHover={{ scale: 1.05, borderColor: "var(--clr-primary-a0)" }}
+        whileTap={{ scale: 0.95 }}
       >
-        {colorScheme === "light" ? <ImSun size={20} /> : <BiMoon size={20} />}
-      </Button>
+        {colorSchemes.map((scheme) => (
+          <motion.div key={scheme} className={clsx(styles.button)}>
+            {colorScheme === scheme && (
+              <motion.div
+                layoutId="color-scheme-indicator"
+                className={styles.indicator}
+                transition={{
+                  type: "spring",
+                  stiffness: 500,
+                  damping: 50,
+                }}
+              >
+                {scheme === "light" ? <ImSun /> : <BiMoon />}
+              </motion.div>
+            )}
+            <div className={styles.placeholder} />
+          </motion.div>
+        ))}
+      </motion.button>
     </Tooltip>
   );
 }
